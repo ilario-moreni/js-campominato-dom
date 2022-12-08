06eb66dfbfa60e5756c2c6e47792861bc874db28
ilario-moreni <ilario2001.mor@gmail.com>
2022-12-08 11:55:28 +0100

    import files (js-campominato-grid) and factorize problem

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/* 1 */
+
+const button = document.getElementById('my-button');
+
+
+
+/* 2 */
+
+/* funzione della generazione della grigla */
+function generateGameGrid(){
+    /* pulisce la tabella se ricliccata */
+    document.querySelector('#my-table').innerHTML = '';
+    /* genera la tabella */
+    for (let i = 0; i < 100; i++){
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.innerText = i+1;
+        cell.addEventListener('click',
+        /*  3 - funzione del toggle delle celle */
+        function(){
+            this.classList.toggle('clicked')
+            console.log(this.innerText)
+            /* inserire qui il check delle bombe */
+        })
+        document.querySelector('#my-table').appendChild(cell)
+    }
+}
+
+button.addEventListener('click', function(){
+    generateGameGrid()
+})
+
+
